6b789d3dd9a4f0c3193272631ed1a4e872f7209d
GreenRamSilgen <kiranshrs@yahoo.com>
2025-01-26 14:50:41 -0800

    Initial commit Experimenting with NavigationStack, Form, Picker, ForEach and @State binings.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount : Int = 0
+    @State private var nameOfUser : String = ""
+    let students : [String] = ["Silg", "Ren", "Daki"]
+    @State private var selectedStudent = "Silg"
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form {
+                Picker ("Select student", selection: $selectedStudent){
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
         }
-        .padding()
+        
+        Form {
+            TextField("Enter your name: ", text: $nameOfUser)
+            Text("Your name: \(nameOfUser)")
+        }
+        
+        Form {
+            ForEach(0..<100) {
+                Text("Num: \($0)")
+            }
+        }
+        
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
+        }
+        
     }
 }
 
