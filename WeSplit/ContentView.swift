236d953ac9b541116404f692a20b0fc6a0bae87d
GreenRamSilgen <kiranshrs@yahoo.com>
2025-01-27 19:55:28 -0800

    Added functionality of app by using: NavigationStack, Form, Section, Picker(.segmented, .navigationLink), @FocusState, keyboardType

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,38 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount : Int = 0
-    @State private var nameOfUser : String = ""
-    let students : [String] = ["Silg", "Ren", "Daki"]
-    @State private var selectedStudent = "Silg"
-    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused : Bool
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson : Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount/100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
     
     var body: some View {
-        NavigationStack{
+        NavigationStack {
             Form {
-                Picker ("Select student", selection: $selectedStudent){
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100, id: \.self) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                 
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
             }
         }
-        
-        Form {
-            TextField("Enter your name: ", text: $nameOfUser)
-            Text("Your name: \(nameOfUser)")
-        }
-        
-        Form {
-            ForEach(0..<100) {
-                Text("Num: \($0)")
-            }
-        }
-        
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
-        }
-        
     }
 }
 
